1b098511edc77a241b0f1611bbfba8219674f482
nikopsaros-svg <nikopsaros@gmail.com>
2025-12-20 10:07:30 -0500

    added authentication

--- controFlow/control_flow.js
+++ controFlow/control_flow.js
@@ -41,3 +41,8 @@ switch (userType) {
         userCategory ="Unknown";
 }
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication status:", authenticationStatus);
