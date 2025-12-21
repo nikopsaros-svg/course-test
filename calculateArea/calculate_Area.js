0f7acdf01e82aa397f063d67d1d2a00be244f8c2
nikopsaros-svg <nikopsaros@gmail.com>
2025-12-21 11:21:49 -0500

    fixed code

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,10 +2,9 @@ let length;
 let width;
 
 function calculateArea() {
-    length = parsefloat(document.getElementById('length').value);
-    width = parsefloat(document.getElementById('width').value);
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
 
-    let area = length * width;
-
-    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+let area = length * width;
+ document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
